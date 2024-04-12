b933ffd657faaf7fcf31edcef7638b71aa4ad860
Diego Coss <Diegocoss999@gmail.com>
2024-04-12 11:16:28 -0700

    Base website. With simple formatting

---
+++ src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { CommonModule, DOCUMENT } from '@angular/common';
+import { Component, Inject } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, CommonModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'online-cv';
+
+  // constructor(@Inject(DOCUMENT) private document: Document) {
+  // }
+}
